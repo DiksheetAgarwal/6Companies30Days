0906014aa6f9bedc7e604972200d85bd2973a35e
Diksheet Agarwal <dksheetagarwal12@gmail.com>
2022-01-06 23:00:28 +0530

    GS Q12

---
+++ Goldman_Sachs/NN check board.cpp
@@ -0,0 +1,29 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+class Solution {
+  public:
+    long long squaresInChessBoard(long long N) {
+        // code here
+        long long x = (N *(N+1) * (2*N +1))/6;
+        return x;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long N;
+        
+        cin>>N;
+
+        Solution ob;
+        cout << ob.squaresInChessBoard(N) << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
