f9a7e837f2fd417ee8bd5495a799c55910f3033e
Diksheet Agarwal <dksheetagarwal12@gmail.com>
2022-01-08 00:05:12 +0530

    Amazon Q3

---
+++ Amazon/Deque sliding window(IPL match).cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution {
+  public:
+    vector<int> max_of_subarrays(vector<int> arr, int n, int k) {
+        // your code here
+        deque<int>q;
+        vector<int> ans;
+        
+        for(int i=0;i<n;i++){
+            
+            if(!q.empty() && q.front() == i-k) q.pop_front();
+            while(!q.empty() && arr[q.back()] < arr[i] ) q.pop_back();
+            q.push_back(i);
+            if(i>=k-1)  ans.push_back(arr[q.front()]);
+        }
+        
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+
+        int n, k;
+        cin >> n >> k;
+
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++) cin >> arr[i];
+        Solution ob;
+        vector<int> res = ob.max_of_subarrays(arr, n, k);
+        for (int i = 0; i < res.size(); i++) cout << res[i] << " ";
+        cout << endl;
+    }
+
+    return 0;
+}  // } Driver Code Ends
