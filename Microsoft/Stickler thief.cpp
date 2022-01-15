e3d5df0061d72fb533a3a30c4be49d6403a9b4ff
Diksheet Agarwal <dksheetagarwal12@gmail.com>
2022-01-15 23:57:40 +0530

    Microsoft q10

---
+++ Microsoft/Stickler thief.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+ // } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find the maximum money the thief can get.
+    int FindMaxSum(int arr[], int n){
+        if(n== 1)
+            return arr[0];
+        int dp[n];
+        dp[0]= arr[0];
+        dp[1]= max(arr[0], arr[1]);
+        for(int i=2;i<n;i++){
+            dp[i] = max(dp[i-1], arr[i]+dp[i-2]);
+        }
+        return dp[n-1];
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    //taking total testcases
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    //taking number of houses
+		int n;
+		cin>>n;
+		int a[n];
+		
+		//inserting money of each house in the array
+		for(int i=0;i<n;++i)
+			cin>>a[i];
+		Solution ob;
+		//calling function FindMaxSum()
+		cout<<ob.FindMaxSum(a,n)<<endl;
+	}
+	return 0;
+}
+  // } Driver Code Ends
