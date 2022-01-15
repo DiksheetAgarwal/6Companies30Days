cc9a14c32174b0791564bf82748e239f214fa354
Diksheet Agarwal <dksheetagarwal12@gmail.com>
2022-01-15 23:19:28 +0530

    Microsoft q9

---
+++ Microsoft/Count Number of SubTrees having given Sum.cpp
@@ -0,0 +1,143 @@
+using namespace std;
+
+// Tree Node
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+    
+    return temp;
+}
+
+// Function to Build Tree
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+        
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+        
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        // Get the current node's value from the string
+        string currVal = ip[i];
+            
+        // If the left child is not null
+        if(currVal != "N") {
+                
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+            
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+            
+        // If the right child is not null
+        if(currVal != "N") {
+                
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
+
+// Your are required to complete this function
+int countSubtreesWithSumX(Node* root, int x);
+
+int main()
+{
+  int t;
+  cin>>t;
+  getchar();
+  while (t--)
+  {
+     string s;
+     getline(cin,s);
+     Node* root = buildTree(s);
+     
+     int x;
+     cin>>x;
+     getchar();
+     cout << countSubtreesWithSumX(root, x)<<endl;
+  }
+  return 0;
+}
+
+// } Driver Code Ends
+
+
+//User function Template for C++
+/*
+Structure of the node of the binary tree is as
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+*/
+int func(Node* root, int X, int &ans){
+    // if(sum == X)
+    //     ans++;
+    if(root == NULL)
+        return 0;
+    int l = func(root->left,X, ans);
+    int r = func(root->right, X, ans);
+    
+    if(l+r+root->data == X)
+    ans++;
+    
+    return (l+r+root->data);
+}
+
+//Function to count number of subtrees having sum equal to given sum.
+int countSubtreesWithSumX(Node* root, int X)
+{
+	// Code here
+	int ans=0;
+	int sum=0;
+	func(root,X, ans);
+	return ans;
+}
