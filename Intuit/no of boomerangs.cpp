e7b25e42f9b5c1b1240f580733a7e5c9428a62f0
Diksheet Agarwal <dksheetagarwal12@gmail.com>
2022-01-26 00:39:00 +0530

    Intuit Q9

---
+++ Intuit/no of boomerangs.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int numberOfBoomerangs(vector<vector<int>>& points) {
+        int n = points.size();
+        int ans=0;
+        
+        for(int i=0;i<n;i++){
+            unordered_map<int, int>v(n);
+            for(int j=0;j<n;j++){
+                int dis = (points[i][1]- points[j][1])*(points[i][1]- points[j][1]) + abs(points[i][0] - points[j][0])* abs(points[i][0] - points[j][0]);
+                // if(v[i].find(dis) == v[i].end())  v[i][dis] = 1;
+                 v[dis]++;
+            }
+            for(auto x:v){
+                if(x.second>1)
+                    ans+= (x.second)*(x.second-1);
+            }
+            
+        }
+    
+        
+        return ans;
+        
+    }
+};
