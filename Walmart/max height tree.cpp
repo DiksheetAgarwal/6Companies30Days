0a11aedcd1ddba8f1b191fe1d5f6a2ff6932702a
Diksheet Agarwal <dksheetagarwal12@gmail.com>
2022-01-28 01:12:58 +0530

    Walmart

---
+++ Walmart/max height tree.cpp
@@ -0,0 +1,37 @@
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    int height(int N){
+        int ans =0;
+        int i=1;
+        if(N == 1)  return 1;
+        while(N>=i){
+            ans++;
+            N-=i;
+            i++;
+        }
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        
+        Solution ob;
+        cout<<ob.height(N)<<endl;
+    }
+    return 0;
+}  // } Driver Code Ends
