fe09d581edd8f7f36161f23752a5089645e53c4d
Diksheet Agarwal <dksheetagarwal12@gmail.com>
2022-01-31 14:06:57 +0530

    Walmart 13

---
+++ Walmart/Kth Largest element in an array.cpp
@@ -0,0 +1,19 @@
+class GreaterNumString {
+    public:
+        bool operator () (string &a, string &b) {
+            if (a.size() != b.size())   return a.size() > b.size();
+            return a > b;
+        }
+};
+
+class Solution {
+public:
+    string kthLargestNumber(vector<string>& nums, int k) {
+        priority_queue <string, vector <string>, GreaterNumString> pq;
+        for (string &x: nums) {
+            pq.push(x);
+            if (pq.size() > k)  pq.pop();
+        }
+        return pq.top();
+    }
+};
